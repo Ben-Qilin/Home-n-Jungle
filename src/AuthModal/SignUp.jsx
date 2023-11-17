ee1e46171a4fb301625ff31d082b6622d04e29b3
benjamin verdès <verdes.benjamin@gmail.com>
2023-11-17 15:45:49 +0100

    webkit

--- src/AuthModal/SignUp.jsx
+++ src/AuthModal/SignUp.jsx
@@ -1,6 +1,6 @@
 const SignUp = () => {
     return (
-        <form style={{boxSizing: 'border-box', backgroundColor: 'rgba(255, 255, 255, 0.45)',backdropFilter: 'blur(10px)', borderRadius: '1rem', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent:'space-evenly' , height: '100%', padding: '0.5rem' }}>
+        <form style={{boxSizing: 'border-box', backgroundColor: 'rgba(255, 255, 255, 0.45)',backdropFilter: 'blur(10px)', borderRadius: '1rem', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent:'space-evenly' , height: '100%', padding: '0.5rem', WebkitFilter: 'blur(10px)' }}>
             <fieldset style={{height: '2rem', width: '100%', display: 'flex', justifyContent: 'space-evenly'}}>
                 <label style={{width: '30%'}}>Nom:</label>
                 <input style={{width: '50%', height: '2rem', borderRadius: '0.5rem', border: '1px solid grey', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}></input>
