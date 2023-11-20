8d8188c931aca6cfca6d960088bbd39f9677d420
benjamin verdès <verdes.benjamin@gmail.com>
2023-11-20 09:07:58 +0100

    add formFetch

--- src/AuthModal/SignIn.jsx
+++ src/AuthModal/SignIn.jsx
@@ -1,13 +1,30 @@
 const SignIn = () => {
+    const [formSign, setFormSign] = ({
+        email: "",
+        password: "",
+    })
+    const [error, setError] = ('')
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+    }
+
+    const handleChange = (event) => {
+        setFormSign({
+            ...formSign,
+            [event.target.name]: event.target.value
+        });
+    };
     return (
-        <form style={{boxSizing: 'border-box', width: '100%', backgroundColor: 'rgba(255, 255, 255, 0.45)',backdropFilter: 'blur(10px)', borderRadius: '1rem', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px',display: 'flex', flexDirection: 'column',alignItems: 'center', justifyContent:'space-evenly', height: '100%',padding: '0.5rem',WebkitFilter: 'blur(10px)'}}>
+        <form onSubmit={handleSubmit} style={{boxSizing: 'border-box', width: '100%', backgroundColor: 'rgba(255, 255, 255, 0.45)',backdropFilter: 'blur(10px)', borderRadius: '1rem', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px',display: 'flex', flexDirection: 'column',alignItems: 'center', justifyContent:'space-evenly', height: '100%',padding: '0.5rem'}}>
             <fieldset style={{height: '2rem', width: '100%', display: 'flex', justifyContent: 'space-evenly'}}>
             <label style={{width: '30%'}}>Email:</label>
-            <input style={{width: '50%', height: '2rem', borderRadius: '0.5rem', border: '1px solid grey', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}></input>
+            <input name="email" value={formSign.email} onChange={handleChange} style={{width: '50%', height: '2rem', borderRadius: '0.5rem', border: '1px solid grey', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}></input>
             </fieldset>
             <fieldset style={{height: '2rem', width: '100%', display: 'flex', justifyContent: 'space-evenly'}}>
                 <label style={{width: '30%'}}>Mot de passe:</label>
-                <input style={{width: '50%', height: '2rem', borderRadius: '0.5rem', border: '1px solid grey', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}></input>
+                <input name="password" value={formSign.password} onChange={handleChange} style={{width: '50%', height: '2rem', borderRadius: '0.5rem', border: '1px solid grey', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}></input>
             </fieldset>
             
             <button type="submit">Se connecter</button>
--- src/AuthModal/SignUp.jsx
+++ src/AuthModal/SignUp.jsx
@@ -1,26 +1,56 @@
+import { useState } from "react";
+
 const SignUp = () => {
+
+    const [form, setForm] = useState({
+        name: "",
+        surname: "",
+        mail: "",
+        password: "",
+        confirmPassword: ""
+    });
+    const [error, setError] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (form.password !== form.confirmPassword) {
+            setError("Les mots de passe de sont pas identiques");
+            return
+        }
+        setError('');
+        console.log(form);
+    }
+
+    const handleChange = (event) => {
+        setForm({
+            ...form,
+            [event.target.name]: event.target.value
+        });
+    };
+
     return (
-        <form style={{boxSizing: 'border-box', backgroundColor: 'rgba(255, 255, 255, 0.45)',backdropFilter: 'blur(10px)', borderRadius: '1rem', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent:'space-evenly' , height: '100%', padding: '0.5rem', WebkitFilter: 'blur(10px)' }}>
+        <form onSubmit={handleSubmit} style={{boxSizing: 'border-box', backgroundColor: 'rgba(255, 255, 255, 0.45)',backdropFilter: 'blur(10px)', borderRadius: '1rem', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent:'space-evenly' , height: '100%', padding: '0.5rem', }}>
             <fieldset style={{height: '2rem', width: '100%', display: 'flex', justifyContent: 'space-evenly'}}>
                 <label style={{width: '30%'}}>Nom:</label>
-                <input style={{width: '50%', height: '2rem', borderRadius: '0.5rem', border: '1px solid grey', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}></input>
+                <input name="name" value={form.name} onChange={handleChange} style={{width: '50%', height: '2rem', borderRadius: '0.5rem', border: '1px solid grey', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}></input>
             </fieldset>
             <fieldset style={{height: '2rem', width: '100%', display: 'flex', justifyContent: 'space-evenly'}}>
                 <label style={{width: '30%'}}>Prénom:</label>
-                <input style={{width: '50%', height: '2rem', borderRadius: '0.5rem', border: '1px solid grey', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}></input>
+                <input name="surname" value={form.surname} onChange={handleChange} style={{width: '50%', height: '2rem', borderRadius: '0.5rem', border: '1px solid grey', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}></input>
             </fieldset>
             <fieldset style={{height: '2rem', width: '100%', display: 'flex', justifyContent: 'space-evenly'}}>
                 <label style={{width: '30%'}}>Mail:</label>
-                <input style={{width: '50%', height: '2rem', borderRadius: '0.5rem', border: '1px solid grey', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}></input>
+                <input name="mail" value={form.mail} onChange={handleChange} style={{width: '50%', height: '2rem', borderRadius: '0.5rem', border: '1px solid grey', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}></input>
             </fieldset>
             <fieldset style={{height: '2rem', width: '100%', display: 'flex', justifyContent: 'space-evenly'}}>
                 <label style={{width: '30%'}}>Mot de passe:</label>
-                <input style={{width: '50%', height: '2rem', borderRadius: '0.5rem', border: '1px solid grey', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}></input>
+                <input type="password" name="password" value={form.password} onChange={handleChange} style={{width: '50%', height: '2rem', borderRadius: '0.5rem', border: '1px solid grey', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}></input>
             </fieldset>
             <fieldset style={{height: '2rem', width: '100%', display: 'flex', justifyContent: 'space-evenly'}}>
                 <label style={{width: '30%'}}>Confirmation de mot de passe:</label>
-                <input style={{width: '50%', height: '2rem', borderRadius: '0.5rem', border: '1px solid grey', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}></input>
+                <input type="password" name="confirmPassword" value={form.confirmPassword} onChange={handleChange} style={{width: '50%', height: '2rem', borderRadius: '0.5rem', border: '1px solid grey', boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}></input>
             </fieldset>
+            {error && <p style={{color: 'red'}}>⚠️{error}</p>}
             
             <button type="submit">S'inscrire</button>
         </form>
